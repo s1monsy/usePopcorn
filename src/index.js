6ce01d590acb9940fea807480b05ed6ce6204e64
DenysPolupan <polupan2004denis@gmail.com>
2026-01-21 15:35:50 +0200

    chore: add PropTypes to StarRating

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
 import StarRating from './StarRating';
 
+function Test() {
+const [movieRating, setMovieRating] = useState(0)
+
+  return (
+    <div>
+      <StarRating color="blue" onSetRating={setMovieRating} maxRating={10}/>
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5}/>
+    <Test/>
+    <StarRating maxRating={5} messages={['Terrible','Bad','Okay','Good','Amazing']}/>
   </React.StrictMode>
 );
